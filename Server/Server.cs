6c761e966a3fffe6815dc494047d92623466d5d0
jmsaRuc <jmsa@ruc.dk>
2024-10-11 21:27:00 +0200

    Add server program and project files

---
+++ Server/Server.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System;
+
+public class Server
+{
+    private readonly int _port;
+
+    public Server(int port)
+    {
+        _port = port;
+
+        
+    }
+
+
+    public void Run() { 
+ 
+        var server = new TcpListener(IPAddress.Loopback, _port); // IPv4 127.0.0.1 IPv6 ::1
+        server.Start();
+
+        Console.WriteLine($"Server started on port {_port}");
+
+        while (true)
+        {
+            var client = server.AcceptTcpClient();
+            Console.WriteLine("Client connected!!!");
+
+            try
+            {
+                var stream = client.GetStream();
+                string msg = ReadFromStream(stream);
+                
+                Console.WriteLine("Message from client: " + msg);
+
+                WriteToStream(stream, msg.ToUpper());
+            }
+            catch { }
+
+        }
+
+    }
+
+    private string ReadFromStream(NetworkStream stream)
+    {
+        var buffer = new byte[1024];
+        var readCount = stream.Read(buffer);
+        return Encoding.UTF8.GetString(buffer, 0, readCount);
+    }
+
+    private void WriteToStream(NetworkStream stream, string msg)
+    {
+        var buffer = Encoding.UTF8.GetBytes(msg);
+        stream.Write(buffer);
+    }
+}
---
+++ Server/ServerProgram.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var server = new Server(5000);
+
+server.Run();
+
+
